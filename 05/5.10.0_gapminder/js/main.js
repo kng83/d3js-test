f0415186cd43be332896963a27443eb857516c09
kng83 <pawel.kng@gmail.com>
2018-11-24 18:58:39 +0100

    2

--- 05/5.10.0_gapminder/js/main.js
+++ 05/5.10.0_gapminder/js/main.js
@@ -14,7 +14,7 @@ let currentDate = 0;
 let flag = true;
 
 //Transition function (powinna byc mniejsza niz nasz update)
-let t = d3.transition().duration(750)
+
 
 let g = d3.select("#chart-area")
     .append("svg")
@@ -31,10 +31,16 @@ let yAxisGroup = g.append("g")
     .attr("class", "y axis");
 
 // X Scale
-let x = d3.scaleLog().range([0, width]);
+let x = d3.scaleLog().range([0, width]).domain([140, 150000]).base(10);
 
 // Y Scale
-let y = d3.scaleLinear().range([height, 0]);
+let y = d3.scaleLinear().range([height, 0]).domain([0, 90]);
+
+let area = d3.scaleLinear()
+    .range([25 * Math.PI, 1500 * Math.PI])
+    .domain([2000, 1400000000]);
+
+let continentColor = d3.scaleOrdinal(d3.schemePastel1);
 
 
 // X Label
@@ -61,23 +67,21 @@ let year = g.append("text")
     .attr("font-size", "20px")
     .attr("text-anchor", "end")
     .attr("class", "year-look")
-    .text("Year");
+    .text("1800");
 
 d3.json("data/data.json").then(data => {
-
-    d3.interval(() => {
+     d3.interval(() => {
         let actuallDate = currentDate + (+data[0].year);
+        currentDate = currentDate >= data.length-1 ? 0 : currentDate + 1;
         updata(data[currentDate].countries)
         g.select("#year").text(actuallDate)
-        currentDate = currentDate + 1;
     }, 100);
-    updata(data);
+
+    updata(data[0].countries);
 });
 
 function updata(data) {
-
-    x.domain([300, 150000]).base(10);
-    y.domain([0, 90]);
+    let t = d3.transition().duration(100);
 
     // X Axis
     let xAxisCall = d3.axisBottom(x).tickSize(3).tickSize(2)
@@ -90,27 +94,25 @@ function updata(data) {
     yAxisGroup.transition(t).call(yAxisCall);
 
     // Join new data with old elements
-    let circles = g.selectAll("circle").data(data, (d) => d);
+    let circles = g.selectAll("circle").data(data, (d) => d.country); // tu jest wazne zeby bylo country
 
     // Exit old elements not present in new data set
     circles.exit()
-    // .attr("fill", "red")
-        .transition(t)
+        .attr("class", "exit")
         .remove();
 
     // Zamieniamy prostokaty na kolka
     // i cy. Usuwamy wysokosc i dajemy polowe pasma tak by kropki byly nad miesiacami
     circles.enter()
         .append("circle")
-        .attr("fill", "blue")
-        .attr("cy", y(0))
-        .attr("cx", d => x(d.income || 3))
-        .attr("r", 2)
+        .attr("class", "enter")
+        .attr("fill", d => continentColor(d.continent))
         //UPDATE
         .merge(circles)
         .transition(t)
-        .attr("cx", d => x(d.income || 3))
-        .attr("cy", d => y(d.life_exp) || 3)
+        .attr("cx", d => x(+d.income || 3))
+        .attr("cy", d => y(+d.life_exp) || 3)
+        .attr("r", d => Math.sqrt(area(d.population) / Math.PI) || 0)
 
 }
 
