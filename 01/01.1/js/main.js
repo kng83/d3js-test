359f279f097e02ce122e4cdd92d9bfe15630ad6e
kng83 <pawel.kng@gmail.com>
2018-11-30 19:09:38 +0100

    soem

--- 01/01.1/js/main.js
+++ 01/01.1/js/main.js
@@ -4,41 +4,27 @@
 *    2.5 - Activity: Adding SVGs to the screen
 */
 
-var svg = d3.select("#chart-area")
+
+let data = [[10,20],[40,50],[100,320]]
+
+let svg = d3.select("#chart-area")
     .append("svg")
     .attr("width", 500)
     .attr("height", 400);
 
-let path = d3.path();
-path.moveTo(10,10);
-path.lineTo(400,400);
+let line = d3.line()
+    .interpolate('linear')
+    .x(function(d){ return x(d.x) })
+    .y(function(d){ return y(d.y) });
 
-svg.append('path').attr('d',path)
+svg.append("path")
+    .attr("class", "line")
+    .attr("d", line);
+
+let path =d3.path()
+    .attr("stroke-dasharray", totalLength + " " + totalLength)
+    .attr("stroke-dashoffset", totalLength)
     .transition()
     .duration(2000)
-   // .ease("linear")
-    .attr("stroke", "orange")
-    .attr("stroke-width", 5);
-
-
-svg.append("line")
-    .attr("x1", 20)
-    .attr("y1", 70)
-    .attr("x2", 100)
-    .attr("y2", 350)
-    .attr("stroke", "brown")
-    .attr("stroke-width", 5);
-
-svg.append("rect")
-    .attr("x", 200)
-    .attr("y", 50)
-    .attr("width", 240)
-    .attr("height", 120)
-    .attr("fill", "blue")
-
-svg.append("ellipse")
-    .attr("cx", 300)
-    .attr("cy", 300)
-    .attr("rx", 50)
-    .attr("ry", 70)
-    .attr("fill", "yellow");
+    .ease("linear")
+    .attr("stroke-dashoffset", 0);
